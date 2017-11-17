85b90b2217c40338d19d4cad9efda56196ad322c
Peter Stockwell <peterstockwell94@gmail.com>
2017-11-18 03:07:36 +1030

    added pagination for blog index page, next/previous post links at the end of a post, removed descriptions from gallery tiles, modified layout of thanks page, decreased splash page image sizes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,3 +3,27 @@ $(document).ready(function() {
     return this.hostname != window.location.hostname;
   }).attr('target', '_blank');
 });
+
+$(document).ready(function(){
+  $(".load-more-posts").click(function() {
+    $(this).addClass("loading");
+
+    var _this = this;
+    var blogList = $("#blog-list > ul");
+    var nextPage = parseInt(blogList.attr("data-currentPage")) + 1;
+    var url = blogList.attr("data-url") + nextPage
+
+    $.get(url, function (response) {
+      var htmlResponse = $.parseHTML(response);
+      var blogItems = $(htmlResponse).find("#blog-list a");
+
+      blogList.attr("data-currentPage", nextPage).append(blogItems);
+
+      if (blogList.attr("data-totalPages") == nextPage) {
+        $(".load-more-posts").remove();
+      }
+
+      $(_this).removeClass("loading");
+    });
+  });
+});
